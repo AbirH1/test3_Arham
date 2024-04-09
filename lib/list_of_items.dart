90a8494b807f676d321b4c6ea21fb17e64e0c140
AbirH1 <hossainabir7709@gmail.com>
2024-04-09 07:41:46 +0400

    updated list of items

---
+++ lib/list_of_items.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show FilteringTextInputFormatter;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Grocery List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: GroceryListPage(),
+    );
+  }
+}
+
+class GroceryListPage extends StatefulWidget {
+  @override
+  _GroceryListPageState createState() => _GroceryListPageState();
+}
+
+class _GroceryListPageState extends State<GroceryListPage> {
+  final List<Map<String, dynamic>> _groceryItems = [
+    {
+      'name': 'FANJUICE',
+      'description': 'Jus d\'Orange Nature',
+      'expiryDate': DateTime.parse('2024-02-12'),
+      'quantity': 2,
+    },
+    {
+      'name': 'BPL',
+      'description': 'Lait écrémé',
+      'expiryDate': DateTime.parse('2024-02-12'),
+      'quantity': 1,
+    },
+    {
+      'name': 'TOUCH\'AM',
+      'description': 'Lotion Aloe Vera',
+      'expiryDate': DateTime.parse('2024-02-12'),
+      'quantity': 0,
+    },
+  ];
+
+  final List<TextEditingController> _controllers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < _groceryItems.length; i++) {
+      _controllers.add(
+          TextEditingController(text: _groceryItems[i]['quantity'].toString()));
+    }
+  }
+
+  void _incrementQuantity(int index) {
+    setState(() {
+      _groceryItems[index]['quantity']++;
+      _controllers[index].text = _groceryItems[index]['quantity'].toString();
+    });
+  }
+
+  void _decrementQuantity(int index) {
+    setState(() {
+      if (_groceryItems[index]['quantity'] > 0) {
+        _groceryItems[index]['quantity']--;
+        _controllers[index].text = _groceryItems[index]['quantity'].toString();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => true, // Handle back button press
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Grocery Item List'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: _groceryItems.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 4,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: Text(
+                    _groceryItems[index]['name'][0].toUpperCase(),
+                    style: TextStyle(color: Color.fromARGB(255, 247, 249, 248)),
+                  ),
+                ),
+                title: Text(
+                  _groceryItems[index]['name'],
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(_groceryItems[index]['description']),
+                    Text(
+                      'Expiry Date: ${_groceryItems[index]['expiryDate'].month}/${_groceryItems[index]['expiryDate'].day}/${_groceryItems[index]['expiryDate'].year}',
+                      style: TextStyle(fontSize: 12, color: Colors.red),
+                    ),
+                  ],
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.remove),
+                      onPressed: () {
+                        _decrementQuantity(index);
+                      },
+                    ),
+                    SizedBox(width: 8),
+                    SizedBox(
+                      width: 40,
+                      child: TextField(
+                        controller: _controllers[index],
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        _incrementQuantity(index);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+}
